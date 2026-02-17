1b685937896cb48bb6fbef24003a5123845f5bb7
Moses Mugera <mugera.developer@gmail.com>
2026-02-17 10:04:25 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,11 @@
+// Configuration file - This should be kept secure on your server
+// For production, load this file from a server-side endpoint
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCOb4BqfiN3vt1Y13UTEpA_boCqNRH6rrQ",
+    authDomain: "dsog-stores-dashboard.firebaseapp.com",
+    databaseURL: "https://dsog-stores-dashboard-default-rtdb.firebaseio.com",
+    projectId: "dsog-stores-dashboard",
+    storageBucket: "dsog-stores-dashboard.firebasestorage.app",
+    messagingSenderId: "1085972125142",
+    appId: "1:1085972125142:web:9abea9ee0dd792a052c077"
+};
